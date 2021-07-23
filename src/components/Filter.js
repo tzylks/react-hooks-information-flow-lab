234fba27773b0f444cbd49c8303afb8f3d2d6c82
TrevorZylks <trevorzylks@gmail.com>
2021-07-22 21:18:07 -0700

    Complete

---
+++ src/components/Filter.js
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Item from "./Item"
+
+
+function Filter({onCategoryChange}){
+    return(
+    <select name="filter" onChange={onCategoryChange}>
+      <option value="All">Filter by category</option>
+      <option value="Produce">Produce</option>
+      <option value="Dairy">Dairy</option>
+      <option value="Dessert">Dessert</option>
+    </select>
+    )
+}
+  
+
+
+export default Filter
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+
+function Header( {isDarkMode, onDarkModeClick }) {
+    return (
+        <header>
+        <h2>Shopster</h2>
+        <button onClick={onDarkModeClick}>
+          {isDarkMode ? "Dark" : "Light"} Mode
+        </button>
+      </header>
+    )
+}
+
+export default Header
